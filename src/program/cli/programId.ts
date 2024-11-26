038e5b47bfd44477fd68e567034bb7ee5880aca0
Pham Tu <tubackkhoa@gmail.com>
2024-11-26 11:24:41 +0700

    update env

--- src/program/cli/programId.ts
+++ src/program/cli/programId.ts
@@ -1,9 +1,7 @@
-import { PublicKey } from "@solana/web3.js"
+import { PublicKey } from '@solana/web3.js';
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
-export const PROGRAM_ID_IDL = new PublicKey(
-  "7UD44gbLC1bBUQABC11igH2rhogmLRcqrDBKNaDsfjN5"
-)
+export const PROGRAM_ID_IDL = new PublicKey(process.env.NEXT_PUBLIC_PROGRAM_ID || '7UD44gbLC1bBUQABC11igH2rhogmLRcqrDBKNaDsfjN5');
 
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
-export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL
+export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL;
