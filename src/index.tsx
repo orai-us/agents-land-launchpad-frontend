f476ea60518e5bfcac9544d55a4212dc70c3fd5c
Pham Tu <tubackkhoa@gmail.com>
2024-12-10 10:59:25 +0700

    remove redundant files

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import 'dotenv/config';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
-ReactDOM.createRoot(document.body).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
       <Header></Header>
