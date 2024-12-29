67771fad85f55968cbb061d244bbf17478189ced
quangdz1704 <Ntq.1704@gmail.com>
2024-12-29 21:21:27 +0700

    fix: udpate chart

--- src/components/trading/hooks/useListenEventSwapChart.tsx
+++ src/components/trading/hooks/useListenEventSwapChart.tsx
@@ -39,109 +39,109 @@ const useListenEventSwapChart = ({ coin }) => {
   }, [coin?._id]);
 
   // subscribe to real-time swap txs on trade
-  useEffect(() => {
-    if (isEmpty(coin)) return;
+  // useEffect(() => {
+  //   if (isEmpty(coin)) return;
 
-    const connection = new Connection(endpoint, {
-      commitment: commitmentLevel,
-      wsEndpoint: import.meta.env.VITE_SOLANA_WS,
-    });
+  //   const connection = new Connection(endpoint, {
+  //     commitment: commitmentLevel,
+  //     wsEndpoint: import.meta.env.VITE_SOLANA_WS,
+  //   });
 
-    // // const listener = new AgentsLandEventListener(connection);
-    // // listener.setProgramEventCallback(
-    // //   "swapEvent",
-    // //   (result: ResultType) => {
-    // //     if (coin.token !== result.mint) {
-    // //       return;
-    // //     }
+  //   // // const listener = new AgentsLandEventListener(connection);
+  //   // // listener.setProgramEventCallback(
+  //   // //   "swapEvent",
+  //   // //   (result: ResultType) => {
+  //   // //     if (coin.token !== result.mint) {
+  //   // //       return;
+  //   // //     }
 
-    // //     const newPrice = calculateTokenPrice(
-    // //       result.tokenReserves,
-    // //       result.lamportReserves,
-    // //       coin.decimals
-    // //       //   solPrice
-    // //     );
-    // //     setCurPrice(newPrice);
+  //   // //     const newPrice = calculateTokenPrice(
+  //   // //       result.tokenReserves,
+  //   // //       result.lamportReserves,
+  //   // //       coin.decimals
+  //   // //       //   solPrice
+  //   // //     );
+  //   // //     setCurPrice(newPrice);
 
-    // //     // socketChart
-    // //     console.log("======listen socket chart=====>>>", result);
-    // //     const newPriceUsd = calculateTokenPrice(
-    // //       result.tokenReserves,
-    // //       result.lamportReserves,
-    // //       SPL_DECIMAL,
-    // //       Number(solPrice)
-    // //     );
+  //   // //     // socketChart
+  //   // //     console.log("======listen socket chart=====>>>", result);
+  //   // //     const newPriceUsd = calculateTokenPrice(
+  //   // //       result.tokenReserves,
+  //   // //       result.lamportReserves,
+  //   // //       SPL_DECIMAL,
+  //   // //       Number(solPrice)
+  //   // //     );
 
-    // //     const tradeTime = new Date().getTime() / 1000;
-    // //     const priceUpdates = {
-    // //       price: newPriceUsd,
-    // //       ts: tradeTime,
-    // //     };
-    // //     const tokenId = result.mint;
+  //   // //     const tradeTime = new Date().getTime() / 1000;
+  //   // //     const priceUpdates = {
+  //   // //       price: newPriceUsd,
+  //   // //       ts: tradeTime,
+  //   // //     };
+  //   // //     const tokenId = result.mint;
 
-    // //     const state = queryClient.getQueryState<RawChart[]>([
-    // //       "chartTable",
-    // //       result.mint,
-    // //     ]);
+  //   // //     const state = queryClient.getQueryState<RawChart[]>([
+  //   // //       "chartTable",
+  //   // //       result.mint,
+  //   // //     ]);
 
-    // //     if (!state || !state.data || !newPriceUsd) {
-    // //       return;
-    // //     }
-    // //     // console.log(
-    // //     //   "SOCKET :>> tokenId, priceUpdates",
-    // //     //   result.mint,
-    // //     //   newPriceUsd
-    // //     // );
+  //   // //     if (!state || !state.data || !newPriceUsd) {
+  //   // //       return;
+  //   // //     }
+  //   // //     // console.log(
+  //   // //     //   "SOCKET :>> tokenId, priceUpdates",
+  //   // //     //   result.mint,
+  //   // //     //   newPriceUsd
+  //   // //     // );
 
-    // //     const priceHistory = [...state.data, priceUpdates];
-    // //     const subscriptionItem = channelToSubscription.get(result.mint);
-    // //     if (!subscriptionItem) {
-    // //       return;
-    // //     }
+  //   // //     const priceHistory = [...state.data, priceUpdates];
+  //   // //     const subscriptionItem = channelToSubscription.get(result.mint);
+  //   // //     if (!subscriptionItem) {
+  //   // //       return;
+  //   // //     }
 
-    // //     const dataChartTable = genOhlcData({
-    // //       priceHistory,
-    // //       range: Number(subscriptionItem?.resolution),
-    // //     });
+  //   // //     const dataChartTable = genOhlcData({
+  //   // //       priceHistory,
+  //   // //       range: Number(subscriptionItem?.resolution),
+  //   // //     });
 
-    // //     const bars = dataChartTable.map((bar) => ({
-    // //       ...bar,
-    // //       time: bar.time * 1000, // Convert from seconds to milliseconds
-    // //     }));
+  //   // //     const bars = dataChartTable.map((bar) => ({
+  //   // //       ...bar,
+  //   // //       time: bar.time * 1000, // Convert from seconds to milliseconds
+  //   // //     }));
 
-    // //     const lastBar = bars[bars.length - 1];
-    // //     let bar: Bar =
-    // //       lastBar.close === lastBar.open && lastBar.high === lastBar.low
-    // //         ? bars[bars.length - 2]
-    // //         : lastBar;
-    // //     // console.log("bar", bar, lastBar);
+  //   // //     const lastBar = bars[bars.length - 1];
+  //   // //     let bar: Bar =
+  //   // //       lastBar.close === lastBar.open && lastBar.high === lastBar.low
+  //   // //         ? bars[bars.length - 2]
+  //   // //         : lastBar;
+  //   // //     // console.log("bar", bar, lastBar);
 
-    // //     if (!bar) return;
-    // //     subscriptionItem.lastBar = bar;
+  //   // //     if (!bar) return;
+  //   // //     subscriptionItem.lastBar = bar;
 
-    // //     // Send data to every subscriber of that symbol
-    // //     subscriptionItem.handlers.forEach((handler) => handler.callback(bar));
+  //   // //     // Send data to every subscriber of that symbol
+  //   // //     subscriptionItem.handlers.forEach((handler) => handler.callback(bar));
 
-    // //     queryClient.setQueryData(
-    // //       ["chartTable", tokenId],
-    // //       (oldData: RawChart[]) => {
-    // //         return [...(oldData || []), priceUpdates];
-    // //       }
-    // //     );
-    // //   },
-    // //   []
-    // // );
+  //   // //     queryClient.setQueryData(
+  //   // //       ["chartTable", tokenId],
+  //   // //       (oldData: RawChart[]) => {
+  //   // //         return [...(oldData || []), priceUpdates];
+  //   // //       }
+  //   // //     );
+  //   // //   },
+  //   // //   []
+  //   // // );
 
-    // const { program, listenerIds } = listener.listenProgramEvents(
-    //   new PublicKey(PROGRAM_ID).toBase58()
-    // );
+  //   // const { program, listenerIds } = listener.listenProgramEvents(
+  //   //   new PublicKey(PROGRAM_ID).toBase58()
+  //   // );
 
-    // return () => {
-    //   if (!program) return;
-    //   console.log("socket-chart---ready to remove listeners");
-    //   Promise.all(listenerIds.map((id) => program.removeEventListener(id)));
-    // };
-  }, [coin?._id]);
+  //   // return () => {
+  //   //   if (!program) return;
+  //   //   console.log("socket-chart---ready to remove listeners");
+  //   //   Promise.all(listenerIds.map((id) => program.removeEventListener(id)));
+  //   // };
+  // }, [coin?._id]);
 
   return {
     curPrice,
