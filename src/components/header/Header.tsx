6487fc5e1681fec3951df7c141906e0141319df8
quangdz1704 <Ntq.1704@gmail.com>
2024-12-29 21:18:18 +0700

    fix: update rpc"

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -161,7 +161,7 @@ const Header: FC = () => {
         isOpen={showStepWork}
         closeModal={() => setShowStepWork(false)}
       />
-      <MarqueeToken />
+      {/* <MarqueeToken /> */}
       <header className="relative z-10 w-full h-[72px] md:h-[96px] bg-[#13141D] m-auto flex justify-center items-center border-b border-solid border-[rgba(88,90,107,0.24)]">
         <div className="py-6 px-2 flex justify-between items-center max-w-[1216px] w-full h-full">
           <div className="flex gap-2 items-center">
